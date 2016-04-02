298bb271bfd147ad50a9e4790c5cfdd88412b42e
Lucas Torroba <lucasfth@gmail.com>
2016-04-02 09:46:25 -0300

    Updated index.android.js
    Exact copy of the iOS files.

--- index.android.js
+++ index.android.js
@@ -1,51 +1,140 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+'use strict';
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+var React = require('react-native');
+var {
+    StyleSheet,
+    Text,
+    View,
+    Component,
+    Dimensions,
+    Image,
+    TouchableWithoutFeedback
+} = React;
 
-class ForaDilma extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var ReactART = require('ReactNativeART');
+var {
+    LinearGradient,
+    RadialGradient,
+    Pattern,
+    Transform,
+    Path,
+    Surface,
+    Group,
+    ClippingRectangle,
+    Shape,
+} = ReactART;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var {height, width} = Dimensions.get('window');
+
+var styles = StyleSheet.create({
+    text: {
+        color: 'black',
+        fontSize: 30,
+        margin: 80
+    },
+    container: {
+        position: 'relative',
+        flex: 1,
+        backgroundColor: '#2c3e50'
+    },
+    level: {
+        backgroundColor: '#27ae60',
+        width: width,
+        height: 80,
+        margin: 0,
+        padding: 0
+    },
+    levelHighlight: {
+        position: 'absolute',
+        backgroundColor: '#2ecc71',
+        width: 100,
+        height: 80
+    },
+    levelText: {
+        marginTop: 20,
+        color: 'black',
+        fontSize: 30,
+        margin: 0,
+        backgroundColor: 'transparent',
+        textAlign: 'center'
+    },
+    hitsText: {
+        color: 'white',
+        fontSize: 60,
+        margin: 0,
+        backgroundColor: 'transparent',
+        textAlign: 'center'
+    },
+    hitsContainer: {
+        position: 'absolute',
+        width: width,
+        bottom: 80
+    },
+    button: {
+        width: (width-40),
+        height: (width-40),
+        marginTop: (height - 80) / 2 - (width - 40) / 2 - 30,
+        marginLeft: 40 / 2
+    }
 });
 
-AppRegistry.registerComponent('ForaDilma', () => ForaDilma);
+class MainButton extends Component {
+    constructor() {
+        super();
+        this.state = {
+            pressed: false
+        }
+    }
+
+    render() {
+        return (
+            <TouchableWithoutFeedback onPressIn={ () => this.toggleButton(true) } onPressOut={ () => this.toggleButton(false) }>
+                <Image source={this.state.pressed ? require('./res/ButtonPressed.png') : require('./res/Button.png') } style={styles.button}/>
+            </TouchableWithoutFeedback>
+        );
+    }
+
+    toggleButton(s:Boolean) {
+        let newState = {...this.state};
+        newState.pressed = s;
+
+        let newMainState = {...this.props.link.state};
+
+        if(s && !this.state.pressed) {
+            newMainState.presses++;
+        }
+
+        this.forceUpdate();
+
+        this.setState(newState);
+        this.props.link.setState(newMainState);
+    }
+}
+
+class ForaDilmaApp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            presses: 200000
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.level}>
+                    <View style={styles.levelHighlight}></View>
+                    <Text style={styles.levelText}>PROTESTANTE MIRIM</Text>
+                </View>
+                <MainButton link={this}/>
+                <View style={styles.hitsContainer}>
+                    <Text style={styles.hitsText}>
+                        {this.state.presses}
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+React.AppRegistry.registerComponent('ForaDilma', function() { return ForaDilmaApp });
